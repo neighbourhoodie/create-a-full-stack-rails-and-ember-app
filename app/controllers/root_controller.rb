5bbff8f27619e6f3c3e2c5f70dcc3bb55f12b4c6
Markus Mühlberger <markus@mmuehlberger.com>
2016-01-02 21:23:31 +0100

    Add frontpage serving

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,4 @@
-class RootController < ApplicationController
+class RootController < ActionController::API
   def index
     current_revision = $redis.get('relationship:index:current')
     render html: $redis.get("relationship:index:#{current_revision}").html_safe
